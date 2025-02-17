1aaf1d02653298bacb41249baf3c2c9f5b8ac564
Karolis44 <karolis.jursenas@gmail.com>
2025-02-17 19:35:01 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './app.css';
+import './buttons.css'
+
+export default function  App() {
+  return (
+    <div className='app'>
+      <div className='app-bin'>
+        <div className='create-bin'>
+          Create
+        </div>
+        <div className='list-bin'>
+          List
+        </div>
+
+      </div>      
+    </div>
+  );
+}
