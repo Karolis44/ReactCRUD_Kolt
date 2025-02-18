7e10fdfaf2b6196f3d17dc1aa1e576af9bcd3153
Karolis44 <karolis.jursenas@gmail.com>
2025-02-18 13:38:04 +0200

    Inputs

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -32,16 +32,27 @@ export default function Create() {
                 <input type="text" name="code" className="kolt-code-ctrl" min={10000000} max={99999999} onChange={handleKolt} value={kolt.code} />
             </div>
             <div>
-                <label className="kolt-busy">IS Busy:</label>
-                <input type="text" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} />
+                <div class="radio">
+                    Is busy
+                    <label class="busy">
+                        <input type="radio" data-group="choice" data-option="yes" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} /> Taip
+                    </label>
+                    <label>
+                        <input type="radio" data-group="choice" data-option="no" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} /> Ne
+                    </label>
+
+                    {/* <input type="radio" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} />   */}
+                </div>
+
+                {/* <input type="radio" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} /> */}
             </div>
             <div>
                 <label className="kolt-date">Last use time date:</label>
-                <input type="text" name="lastusedate" className="kolt-date-ctrl" onChange={handleKolt} value={kolt.lastusedate} />
+                <input type="date" name="lastusedate" className="kolt-date-ctrl" onChange={handleKolt} value={kolt.lastusedate} />
             </div>
             <div>
                 <label >Total Ride Kilometres: <b class="kolt-totalkm">{kolt.totalridekm}km</b></label>
-                <input type="range" name="totalridekm" className="kolt-totalkm-ctrl" min={1} max={5000} step={0.01} onChange={handleKolt} value={kolt.totalridekm} />
+                <input type="range" class="ranger" name="totalridekm" className="kolt-totalkm-ctrl" min={1} max={5000} step={0.01} onChange={handleKolt} value={kolt.totalridekm} />
             </div>
 
             <button className="green" >Įrašyti duomenis</button>
