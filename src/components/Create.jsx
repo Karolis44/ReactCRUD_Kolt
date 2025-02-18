244662418144e63672f44b46ea6c67fc7d164624
Karolis44 <karolis.jursenas@gmail.com>
2025-02-18 21:55:27 +0200

    local

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -5,6 +5,7 @@ export default function Create() {
 
 
     const [kolt, setkolt] = useState(C.defaultKolt);
+    
 
     const handleKolt = e => {
         let value = e.target.value;
@@ -16,46 +17,49 @@ export default function Create() {
         setkolt({ ...kolt, [e.target.name]: value });
     };
 
+    const saveToLocalStorage = () => {
+     
+        let existingData = JSON.parse(localStorage.getItem('koltData'));
+        if (!Array.isArray(existingData)) {
+            existingData = [];
+        }
+    
+        existingData.push(kolt);
 
-
+        localStorage.setItem('koltData', JSON.stringify(existingData));
+    
+        alert('Duomenys išsaugoti!');
+    };
 
     return (
         <div className="create-template">
 
-            <div className="topic"> Suveskite paspirtuko Duomenis</div>
+            <div className="topic"> Įveskite paspirtuko Duomenis</div>
             <div>
-                <label className="kolt-id">ID:</label>
+                <label className="kolt-id">Paspirtuko ID:</label>
                 <input type="text" name="id" className="kolt-id-ctrl" onChange={handleKolt} value={kolt.id} />
             </div>
             <div>
-                <label className="kolt-code">Registration code:</label>
+                <label className="kolt-code">Registracijos kodas:</label>
                 <input type="text" name="code" className="kolt-code-ctrl" min={10000000} max={99999999} onChange={handleKolt} value={kolt.code} />
             </div>
+            
+
             <div>
-                <div class="radio">
-                    Is busy
-                    <label class="busy">
-                        <input type="radio" data-group="choice" data-option="yes" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} /> Taip
-                    </label>
-                    <label>
-                        <input type="radio" data-group="choice" data-option="no" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} /> Ne
-                    </label>
-
-                    {/* <input type="radio" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} />   */}
-                </div>
-
-                {/* <input type="radio" name="busy" className="kolt-busy-ctrl" onChange={handleKolt} value={kolt.busy} /> */}
+                <label className="kolt-busy">Užimtumas:</label>
+                <input type="text" name="busy" className="kolt-busy-ctrl"  onChange={handleKolt} value={kolt.busy} />
             </div>
+               
             <div>
-                <label className="kolt-date">Last use time date:</label>
+                <label className="kolt-date">Paskutinio naudojimo data:</label>
                 <input type="date" name="lastusedate" className="kolt-date-ctrl" onChange={handleKolt} value={kolt.lastusedate} />
             </div>
             <div>
-                <label >Total Ride Kilometres: <b class="kolt-totalkm">{kolt.totalridekm}km</b></label>
-                <input type="range" class="ranger" name="totalridekm" className="kolt-totalkm-ctrl" min={1} max={5000} step={0.01} onChange={handleKolt} value={kolt.totalridekm} />
+                <label >Rida kilometrais: <b className="kolt-totalkm">{kolt.totalridekm}km</b></label>
+                <input type="range"  name="totalridekm" className="kolt-totalkm-ctrl" min={1} max={5000} step={0.01} onChange={handleKolt} value={kolt.totalridekm} />
             </div>
 
-            <button className="green" >Įrašyti duomenis</button>
+            <button className="green" onClick={saveToLocalStorage}>Įrašyti duomenis</button>
 
         </div>
     );
