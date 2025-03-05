0c7095de7ba8c41262c4d8fa298173df0d142b3e
Karolis44 <karolis.jursenas@gmail.com>
2025-03-05 17:24:12 +0200

    id dl

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -5,8 +5,8 @@ export default function Delete({ handleDelete, closeDeleteModal }) {
                 <div className="modal-content">
                     <p>Do you really want to delete this data?</p>
                     <div className="modal-buttons">
-                        <button className="red" onClick={handleDelete}>Taip</button>
-                        <button className="yellow" onClick={closeDeleteModal}>Ne</button>
+                        <button className="red" onClick={handleDelete}>Yes</button>
+                        <button className="yellow" onClick={closeDeleteModal}>No</button>
                     </div>
                 </div>
             </div>
